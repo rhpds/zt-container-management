5ded2ddf68e5b2d741b4d20bf8bb2dcf4574b059
Matthew Yee <myee@redhat.com>
2025-09-05 10:04:49 -0700

    Add Grafana and Cockpit services to instances.yaml, update firewall rules, and enhance documentation for container management

---
+++ setup-automation/setup-rhel.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Enable cockpit functionality in showroom.
+echo "[WebService]" > /etc/cockpit/cockpit.conf
+echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
+echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
+systemctl enable --now cockpit.socket
