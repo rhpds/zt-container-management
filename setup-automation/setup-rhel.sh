1c6c8c074d2c7e924f718e8c161dc5d4f78eb983
Matthew Yee <myee@redhat.com>
2025-09-05 10:44:16 -0700

    Add password configuration for RHEL user in setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -5,3 +5,5 @@ echo "[WebService]" > /etc/cockpit/cockpit.conf
 echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
 echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
 systemctl enable --now cockpit.socket
+
+echo 'rhel:redhat' | chpasswd
