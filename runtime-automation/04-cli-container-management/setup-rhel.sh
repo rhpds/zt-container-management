5dab885a47542754269347f790fff2ead6d8cdf6
Matthew Yee <myee@redhat.com>
2025-09-08 13:40:44 -0700

    Add Grafana images and enhance documentation for GUI and CLI container management, including updated login instructions and terminal tab details.

--- runtime-automation/04-cli-container-management/setup-rhel.sh
+++ runtime-automation/04-cli-container-management/setup-rhel.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 echo "Starting module called 04-cli-container-management" >> /tmp/progress.log
+
+podman container rm --all --force #get trid of the containers made in step 1
+podman image rm --all --force #get rid of the container images used in step 1
